abf0860b30cd4b4162ee35a35112d92f8fdf3642
Julio Guimaraes <juliocefg@gmail.com>
2020-06-23 12:43:12 -0300

    Style changed, UI Functions added

--- app.js
+++ app.js
@@ -12,12 +12,21 @@ loadEventListeners();
 function loadEventListeners() {
     // Add task event
     form.addEventListener('submit', addTask);
+
+    // Remove task event
+    taskList.addEventListener('click', removeTask)
+
+    // Clear task event 
+    clearBtn.addEventListener('click', clearTasks);
+
+    // Filter tasks
+    filter.addEventListener('keyup', filtertasks)
 }
 
 // Add Task 
 function addTask(e) {
     if (taskInput.value === '') {
-        alert('Add a Task');
+        alert('Adicione Uma Tarefa');
     }
     // create li element
     const li = document.createElement('li');
@@ -28,7 +37,7 @@ function addTask(e) {
     const link = document.createElement('a');
     link.className = 'delete-item secondary-content';
     // add Icon
-    link.innerHTML = '<i class="fa fa-remove"></i>';
+    link.innerHTML = '<i class="material-icons">remove</i>';
 
 
     console.log(li);
@@ -42,4 +51,36 @@ function addTask(e) {
 
 
     e.preventDefault();
+}
+
+// Remove Task
+function removeTask(e) {
+    if (e.target.parentElement.classList.contains('delete-item')) {
+        if (confirm('Tem Certeza?')) {
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+
+// Clear Tasks
+function clearTasks() {
+    while (taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+}
+
+// Filter Tasks
+function filtertasks(e) {
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach
+        (function (task) {
+            const item = task.firstChild.textContent;
+            if (item.toLowerCase().indexOf(text) != -1) {
+                task.style.display = 'block';
+            } else {
+                task.style.display = 'none';
+
+            }
+        });
 }
